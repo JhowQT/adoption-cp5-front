bf15305fde06736cdb2387453d7451ae40188d64
JhowQT <jhon.q.torrez@gmail.com>
2026-04-23 05:12:08 -0300

    ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
-import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 
 export const routes: Routes = [
-  { path: '', component: Register }
+  { path: '', component: Login },
+  { path: 'register', component: Register }
 ];
